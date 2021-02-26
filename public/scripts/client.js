8a516f897b05cb77d3f7d5e56b2a9db9bbff18d3
Isamu Ito <isamsito@gmail.com>
2021-02-26 02:16:20 +0000

    format using prettier

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,81 +4,75 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const escape =  function(str) {
-  let div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
-
-
-const createTweetElement = function(tweetData) {
-  let $tweet = `
-    <article class="tweet">
-    <div class="spread-apart">
-      <div class="left">
-        <img class="profile-pic" src="${tweetData.user.avatars}" alt="Profile pic">
-        <span>${tweetData.user.name}</span>
-      </div>
-      <span class="username">${tweetData.user.handle}</span>
-    </div>
-    <p> 
-      ${escape(tweetData.content.text)}
-    </p>
-    <div class="spread-apart tweet-footer">
-      <span>${new Date(tweetData.created_at)}</span>
-      <span>
-        <a href="www.google.com"><i class="fas fa-flag fa-xs"></i></a>
-        <a href="www.google.com"><i class="fas fa-retweet fa-xs"></i></a>
-        <a href="www.google.com"><i class="fas fa-heart fa-xs"></i></a>
-      </span>
-    </div>
-  </article>
-  `
-  return $tweet;
-};
-
-const renderTweets = function(tweets) {
-  $(document).ready(function() {
-    for(let tweet of tweets) {
-      $(createTweetElement(tweet)).insertAfter('.new-tweet');
-    }
-  });
-}
-
-const loadTweets = function() {
-  $.ajax({
-    url: '/tweets',
-    method: 'GET',
-  }).then(renderTweets);
-}
-
-const createWarningMessage = (warning) => {
- return `
- 
- `;
-}
 
 
 $(document).ready(function() {
+  const $newTweet = $('.new-tweet');
+
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  
+  const createTweetElement = function(tweetData) {
+    let $tweet = `
+    <article class="tweet">
+      <div class="spread-apart">
+        <div class="left">
+          <img class="profile-pic" src="${tweetData.user.avatars}" alt="Profile pic">
+          <span>${tweetData.user.name}</span>
+        </div>
+        <span class="username">${tweetData.user.handle}</span>
+      </div>
+      <p> 
+        ${escape(tweetData.content.text)}
+      </p>
+      <div class="spread-apart tweet-footer">
+        <span>${new Date(tweetData.created_at)}</span>
+        <span>
+          <a href="www.google.com"><i class="fas fa-flag fa-xs"></i></a>
+          <a href="www.google.com"><i class="fas fa-retweet fa-xs"></i></a>
+          <a href="www.google.com"><i class="fas fa-heart fa-xs"></i></a>
+        </span>
+      </div>
+    </article>
+    `;
+    return $tweet;
+  };
+  
+  const renderTweets = function(tweets) {
+      for(let tweet of tweets) {
+        // insertAfter prevents tweet order from being reversed.
+        $(createTweetElement(tweet)).insertAfter($newTweet);
+      }
+  };
+  
+  const loadTweets = function() {
+    return $.ajax({
+      url: '/tweets',
+      method: 'GET',
+    })
+  };
+
   $('#create-tweet').on('submit', function(event) {
     event.preventDefault();
-    // add user field in ajax
-    const text = $(this).children('textarea').val();
+    const $form = $(this)
+    const $textArea = $form.find('.tweet-text');
+    const text = $textArea.val();
     const textLength = text.length;
     if(text !== '' && textLength <= 140) {
       $.ajax({
         url: '/tweets',
         method: 'POST',
-        data: $(this).serialize()
+        data: $form.serialize()
       }).then(()=>{
         $('.tweet').remove();
         loadTweets();
       });
-      $(this).children('textarea').val('');
-      $(this).find('output').val('140');
+      $textArea.val('');
+      $form.find('.counter').val('140');
     } else if(textLength > 140){
-      // alert('Too many characters');
-      // $('.container').prepend(createWarningMessage('Please use less that 140 characters')).delay(2000);
       $('#longWarning').fadeIn().delay(2000).fadeOut();
     } else {
       $('#emptyWarning').fadeIn().delay(2000).fadeOut();
@@ -89,6 +83,9 @@ $(document).ready(function() {
     event.preventDefault()
     $('textarea').focus();
   });
-})
+  
+  loadTweets().then(renderTweets);
+});
 
-loadTweets();
+
+
